f292659b176054398d72885fd95ad05f67a088a9
LaraibAzmat <azmatlaraib2003@gmail.com>
2024-07-15 09:47:54 +0500

    responsive home page for mobile screens

--- src/Components/Meals/MealSummary.js
+++ src/Components/Meals/MealSummary.js
@@ -15,7 +15,7 @@ const MealSummary = () => {
             Choose your favorite meal from our broad selection of available meals
             and enjoy a delicious lunch or dinner at home.
           </p>
-          <p>
+          <p className={styles.hidePara}>
             All our meals are cooked with high-quality ingredients, just-in-time and
             of course by experienced chefs!
           </p>
